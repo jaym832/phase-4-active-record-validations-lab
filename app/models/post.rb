39ac3d6cbef1b6c09670f04922bda4e3a63cabef
Javier Marin <jjmarin777@gmail.com>
2021-06-09 11:29:03 -0500

    commit tree f598428c9bf73ca320ac30eecdd2eae49a704680

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction  Non-Fiction)}
+
+    validate :clickbait?
+
+    PATTERNS=[/You Won't Believe These True Facts/i]
+
+    def clickbait?
+        if PATTERNS.none?{|pattern|pattern.match title}
+            errors.add(:title, "is clickbait!")
+        end
+       
+
+    end
+
+
+
 end
